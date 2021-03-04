2c86a6a9a3a88a29e14bed37b72ab10b94542faf
unknown <felipefsc.04@gmail.com>
2021-03-04 19:32:22 -0300

    cadastro produto tela

---
+++ README.md
@@ -0,0 +1,2 @@
+# favy-developt
+trabalho de projeto integrados do quarto semestre do curso de TADS
---
+++ developt/src/main/java/com/fazy/developt/DeveloptApplication.java
@@ -0,0 +1,13 @@
+package com.fazy.developt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloptApplication.class, args);
+	}
+
+}
